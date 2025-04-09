c4318a008e92859734dd8cf9352a07bfed775af0
siva18052006 <sivaprasathsivaprasath123@gmail.com>
2025-04-09 14:42:22 +0530

    Time: 459 ms (5.05%), Space: 121.8 MB (5.12%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,35 +1,40 @@
 class Solution {
-    void func(int index,int sum,int[] nums,ArrayList<Integer> res,int b,int[] check){
-        if(check[0]==1){
-            return;
-        }
-        if(index>=nums.length){
-            if(sum==b){
-                check[0]=1;
+    void func(int index, int sum, int[] nums, ArrayList<Integer> res, int b, int[] check, HashSet<String> memo) {
+        if (check[0] == 1) return;
+
+        // Memoization: skip already processed state
+        String key = index + "," + sum;
+        if (memo.contains(key)) return;
+        memo.add(key);
+
+        if (index >= nums.length) {
+            if (sum == b) {
+                check[0] = 1;
             }
             return;
         }
-         func(index+1,sum+nums[index],nums,res,b,check);
-         func(index+1,sum,nums,res,b,check);
+
+        func(index + 1, sum + nums[index], nums, res, b, check, memo);
+        func(index + 1, sum, nums, res, b, check, memo);
     }
+
     public boolean canPartition(int[] nums) {
-        int count=0,b=0;
-        for(int i=0;i<nums.length;i++){
-            count+=nums[i];
+        int count = 0, b = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i];
         }
-        if(count%2!=0){
+        if (count % 2 != 0) {
             return false;
         }
-        b=count/2;
-        Arrays.sort(nums);
-        ArrayList<Integer> res=new ArrayList<Integer>();
-        int len=nums.length;
-        int middle=len/2;
-        int[] check=new int[1];
-        func(0,0,nums,res,b,check);
-        if(check[0]==1){
-            return true;
-        }
-        return false;
+
+        b = count / 2;
+        Arrays.sort(nums); // optional but can help in pruning
+        ArrayList<Integer> res = new ArrayList<>();
+        int[] check = new int[1];
+
+        HashSet<String> memo = new HashSet<>();
+        func(0, 0, nums, res, b, check, memo);
+
+        return check[0] == 1;
     }
 }
