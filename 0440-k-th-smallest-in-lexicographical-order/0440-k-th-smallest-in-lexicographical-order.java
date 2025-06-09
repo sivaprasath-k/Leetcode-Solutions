67a942039be0d5fe336fbce59a86686d214c710f
siva18052006 <sivaprasathsivaprasath123@gmail.com>
2025-06-09 18:33:09 +0530

    Time: 0 ms (100%), Space: 40.3 MB (77.21%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -1,11 +1,28 @@
 class Solution {
     public int findKthNumber(int n, int k) {
-        List<Integer> ans=new ArrayList<>();
-        for(int i=1;i<=n;i++){
-            ans.add(i);
+        int curr = 1;
+        k = k - 1;
+
+        while (k > 0) {
+            long steps = countSteps(n, curr, curr + 1);
+            if (steps <= k) {
+                curr++;
+                k -= steps;
+            } else {
+                curr *= 10;
+                k--;
+            }
         }
-        Collections.sort(ans,(a,b)->a.toString().compareTo(b.toString()));
-        int fin=ans.get(k-1);
-        return fin;
+        return curr;
+    }
+
+    private long countSteps(int n, long first, long last) {
+        long steps = 0;
+        while (first <= n) {
+            steps += Math.min(n + 1L, last) - first;
+            first *= 10;
+            last *= 10;
+        }
+        return steps;
     }
 }
