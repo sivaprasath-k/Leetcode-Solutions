71c168354ab8e46d814f6b255f34fa1acf826bcd
siva18052006 <sivaprasathsivaprasath123@gmail.com>
2025-06-12 07:55:29 +0530

    Time: 1 ms (98.36%), Space: 43.5 MB (52.79%) - LeetHub

--- 3423-maximum-difference-between-adjacent-elements-in-a-circular-array/3423-maximum-difference-between-adjacent-elements-in-a-circular-array.java
+++ 3423-maximum-difference-between-adjacent-elements-in-a-circular-array/3423-maximum-difference-between-adjacent-elements-in-a-circular-array.java
@@ -2,10 +2,7 @@ class Solution {
     public int maxAdjacentDistance(int[] nums) {
         int res=Math.abs(nums[0]-nums[nums.length-1]);
         for(int i=0;i<nums.length-1;i++){
-            int check=Math.abs(nums[i]-nums[i+1]);
-            if(check>res){
-                res=check;
-            }
+            res = Math.max(res, Math.abs(nums[i] - nums[i + 1]));
         }
         return res;
     }
