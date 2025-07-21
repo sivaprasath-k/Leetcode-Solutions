30389443f27227ba81f4d5b85fd96042e219a039
SIVAPRASATH K <sivaprasathsivaprasath123@gmail.com>
2025-07-21 18:36:50 +0530

    Time: 0 ms (100%), Space: 41.9 MB (37.54%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,27 +1,44 @@
 class Solution {
+    public int check(int i,int j,int[][] board,int row,int column){
+        int count=0;
+
+        if(i-1>=0 && board[i-1][j]==1) count++;
+        if(i+1<row && board[i+1][j]==1) count++;
+
+        if(j-1>=0 && board[i][j-1]==1) count++;
+        if(j+1<column && board[i][j+1]==1) count++;
+
+        if(i+1<row && j-1>=0 && board[i+1][j-1]==1) count++;
+        if(i+1<row && j+1<column && board[i+1][j+1]==1) count++;
+
+        if(i-1>=0 && j-1>=0 && board[i-1][j-1]==1) count++;
+        if(i-1>=0 && j+1<column && board[i-1][j+1]==1) count++;
+
+        return count;
+    }
     public void gameOfLife(int[][] board) {
         int row=board.length;
-        int column=board.length;
+        int column=board[0].length;
         int[][] arr=new int[row][column];
         for(int i=0;i<row;i++){
             for(int j=0;j<column;j++){
                 if(board[i][j]==1){
-                    if(i-1>=0 && i+1<row && j-1>=0 && j+1<column){
-                        int pre=i-1,next=i+1,count=0;
-                        for(int k=0;k<column;k++){
-                            if(board[pre][k]==1 || board[next][k]==1){
-                                count++;
-                            }
-                        }
-                        if(count<2 || count>3) arr[i][j]=0;
+                    int ans=check(i,j,board,row,column);
+                    if(ans!=2 && ans!=3){
+                        arr[i][j]=0;
                     }
-                    else{
-                        int pre=i-1,next=i+1,count=0;
-                        for(int k=0;k<column;k++){
-                            if(pre>=0)
-                        }
-                    }
+                    else arr[i][j]=1;
                 }
+                else{
+                    int ans=check(i,j,board,row,column);
+                    if(ans==3) arr[i][j]=1;
+                    // else arr[i][j]=0;
+                }
+            }
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                board[i][j]=arr[i][j];
             }
         }
     }
