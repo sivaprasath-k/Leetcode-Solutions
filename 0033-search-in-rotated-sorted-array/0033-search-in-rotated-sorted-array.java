f1789977bb607661ed02a7b82555277af7121ef2
SIVAPRASATH K <sivaprasathsivaprasath123@gmail.com>
2025-08-04 21:40:09 +0530

    Time: 0 ms (100%), Space: 42.2 MB (52.19%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int ans=-1;
+        if(nums.length==1){
+            if(nums[0]==target) return 0;
+            else return -1;
+        }
+        if(nums.length==2){
+            for(int i=0;i<2;i++){
+                if(nums[i]==target) return i;
+            }
+            return -1;
+        }
+         int last=0,flag=0;
+         for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
+                last=i;
+                flag=1;
+                break;
+            }
+         }
+         if(flag==0) last=nums.length;
+
+         int left=0,right=last,mid=left+((right-left)/2);
+         while(left<=right){
+            mid=left+((right-left)/2);
+            if(mid>=nums.length) break;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+         }
+
+         left=last+1;
+         right=nums.length;
+         if(left<nums.length){
+            while(left<=right){
+                mid=left+((right-left)/2);
+                if(mid>=nums.length) break;
+                if(nums[mid]==target) return mid;
+                else if(nums[mid]<target){
+                    left=mid+1;
+                }
+                else{
+                right=mid-1;
+                }
+            }
+         }
+         return ans;
+    }
+}
+
+
+// Arrays.sort(nums);
+//         int ans=-1;
+//         int left=0,right=nums.length-1;mid=left+((right-left)/2);
+//         while(left<=right){
+//             mid=left+((right-left)/2);
+//             if(nums[mid]==target) return
+//         }
