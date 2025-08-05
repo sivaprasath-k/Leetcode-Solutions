08e7a726ccb2bcde5f84a128745c4ab779fd820b
SIVAPRASATH K <sivaprasathsivaprasath123@gmail.com>
2025-08-05 06:28:58 +0530

    Time: 0 ms (100%), Space: 43.5 MB (36.9%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,54 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int ans=-1;
+        if(nums.length==1){
+            if(nums[0]==target) return true;
+            else return false;
+        }
+        if(nums.length==2){
+            for(int i=0;i<2;i++){
+                if(nums[i]==target) return true;
+            }
+            return false;
+        }
+         int last=0,flag=0;
+         for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
+                last=i;
+                flag=1;
+                break;
+            }
+         }
+         if(flag==0) last=nums.length;
+
+         int left=0,right=last,mid=left+((right-left)/2);
+         while(left<=right){
+            mid=left+((right-left)/2);
+            if(mid>=nums.length) break;
+            if(nums[mid]==target) return true;
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+         }
+
+         left=last+1;
+         right=nums.length;
+         if(left<nums.length){
+            while(left<=right){
+                mid=left+((right-left)/2);
+                if(mid>=nums.length) break;
+                if(nums[mid]==target) return true;
+                else if(nums[mid]<target){
+                    left=mid+1;
+                }
+                else{
+                right=mid-1;
+                }
+            }
+         }
+         return false;
+    }
+}
