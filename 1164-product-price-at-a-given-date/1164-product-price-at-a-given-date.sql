a225e66a828346e26c5cc61dec235f309ec469fd
SIVAPRASATH K <sivaprasathsivaprasath123@gmail.com>
2026-04-15 12:13:08 +0530

    Time: 1492 ms (5.75%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,17 @@
+select p1.product_id,ifnull(p2.new_price,10) as price 
+from Products as p1
+left join(
+    select product_id,new_price,change_date
+    from Products
+    where change_date<="2019-08-16"
+    -- order by product_id,change_date desc
+) as p2
+join(
+    select product_id,max(change_date) as dat
+    from Products
+    where change_date<="2019-08-16"
+    group by product_id
+) as p3
+on (p3.product_id=p2.product_id and p3.dat=p2.change_date)
+on (p2.product_id=p1.product_id)
+group by p1.product_id;
